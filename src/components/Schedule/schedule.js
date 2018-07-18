43d3b699ce462328ae7b4d4f5bef23aa5b23bd81
Talmage <utilitytitan@gmail.com>
2018-07-18 20:19:58 +0000

    intalled and connected redux devtools

--- src/components/Schedule/schedule.js
+++ src/components/Schedule/schedule.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Gradient from '../gradiant';
 import ScheduleCourse from './scheduleCourse';
+import ProgressTracker from './progressTracker';
+
 class Schedule extends Component{
     render(){
       return(
@@ -10,7 +12,7 @@ class Schedule extends Component{
           <ScheduleCourse title ={'Problem Solving'}/>
           <ScheduleCourse title ={'Problem Solving'}/>
           <ScheduleCourse title ={'Problem Solving'}/>
-         
+         <ProgressTracker />
           <Gradient/>
           
           </div>
