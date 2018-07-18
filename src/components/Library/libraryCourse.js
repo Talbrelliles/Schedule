3b6559cd58949eb3d7ec8108c44df64860c2ab32
Talmage <utilitytitan@gmail.com>
2018-07-18 04:44:08 +0000

    added a bunch of stuff:

---
+++ src/components/Library/libraryCourse.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class LibraryCourse extends Component {
+    render() {
+       
+        return(
+            <div className="library-course">
+            <label className="library-course__title">Problem Solving</label>
+           
+            <div className="library-course__discription">
+            <label>Course discription</label>
+            <p>Let's make a happy little mountain now. Take your time. Speed will come later. You can create the world you want to see and be a part of. You have that power. Son of a gun. I thought today we would make a happy little stream that's just running through the woods here. You can do it.</p>
+            </div>
+            </div>
+            )
+        
+    }
+}
+export default LibraryCourse;
---
+++ src/components/icons.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Icon = (icon) => {
+        return(
+            
+            <div className-"icon">
+            </div>
+            
+            
+            )
+    }
+}
+export default Icon;
