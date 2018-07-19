879b096c1e71dac027f4c6551d1e4a610eed83f5
Talmage <utilitytitan@gmail.com>
2018-07-19 00:02:21 +0000

    I made the content show

--- src/components/Library/library.js
+++ src/components/Library/library.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+
 import * as actions from '../../actions';
+
 import LibraryCourse from './libraryCourse';
 
-class Library extends Component{
-    
-    componentDidMount() {
+class Library extends Component {
+
+    componentWillMount() {
         this.props.fetchCourses()
     }
-    
-    
-    render(){
-      return(
-          <div className="library">
-          
-          <h1 className="library__title">Course Library</h1>
-          <LibraryCourse/>
-           <LibraryCourse/>
-            <LibraryCourse/>
-          </div>
-          )  
-    }    
+
+    renderCourses() {
+      const data = this.props.courses
+
+      return data.map((course, index) => {
+        return <LibraryCourse {...course} key={index}/>
+      })
+    }
+
+    render() {
+       
+        return (
+            <div className="library">
+                <h1 className="library__title">Course Library</h1>
+              { this.renderCourses() }
+            </div>
+        )
+    }
 }
-export default connect(null, actions)(Library);
+
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    }
+}
+
+export default connect(mapStateToProps, actions)(Library);
--- src/components/Library/libraryCourse.js
+++ src/components/Library/libraryCourse.js
@@ -13,9 +13,9 @@ class LibraryCourse extends Component {
            </div>
            <arrow className="library-course__arrow"/>
           <Action className="library-course__action"/>
-            <div className="library-course__discription">
-            <label>Course description</label>
-            <p>Let's make a happy little mountain now. Take your time. Speed will come later. You can create the world you want to see and be a part of. You have that power. Son of a gun. I thought today we would make a happy little stream that's just running through the woods here. You can do it.</p>
+            <div className="library-course__description">
+            <label>{ this.props.title }</label>
+            <p>{ this.props.description }</p>
             </div>
             </div>
         )
